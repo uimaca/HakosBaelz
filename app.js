587dbed6fa59f31deab7e35d3fc8f686a14ac21e
uimaca <163497049+uimaca@users.noreply.github.com>
2024-03-26 17:19:38 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -9,26 +9,3 @@ changeButton.forEach((e) => {
     e.classList.add("active");
   });
 });
-
-//影片預覽左右控制
-//滑鼠滾動左右
-let scrollTurnLeft = document.querySelector(".scrollTurnLeft");
-
-scrollTurnLeft.addEventListener("wheel", (event) => {
-  event.preventDefault();
-  scrollTurnLeft.scrollLeft += event.deltaY;
-});
-
-//容器往左移
-const controlLeft = document.querySelector(".controlLeft");
-
-controlLeft.addEventListener("click", () => {
-  document.querySelector(".scrollTurnLeft").scrollLeft += -330;
-});
-
-//容器往右移
-const controlRight = document.querySelector(".controlRight");
-
-controlRight.addEventListener("click", () => {
-  document.querySelector(".scrollTurnLeft").scrollLeft += 330;
-});
